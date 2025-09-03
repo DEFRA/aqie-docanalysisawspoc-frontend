eff10b59fc7c1f5b80405b3b79fc2a76a2586de0
Saranya <Saranya.Vinayagam@cognizant.com>
2025-09-03 18:12:05 +0100

    excluded test cases to run

--- vitest.config.js
+++ vitest.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         '.public',
         'coverage',
         'postcss.config.js',
-        'stylelint.config.js',
+        'stylelint.config.js'
       ]
     }
   }
